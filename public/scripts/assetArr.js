a97c1c6080ac06935382127fb60b54271984a45a
Ryan Shores <ryan.shores@me.com>
2018-04-30 18:00:52 +0000

    code changes

--- public/scripts/assetArr.js
+++ public/scripts/assetArr.js
@@ -10,6 +10,36 @@ $('.removeBtn').on('click', function(){
    $(this).parent().remove();
 });
 
+$('.addBtn').on('click', function(){
+   var form = $(this).parent();
+   console.log(form);
+   var inputs = form.children('.input-group');
+   console.log(inputs);
+   var asset = {
+      geometry: {
+         coordinates: [ inputs.children('.lng').val(), inputs.children('.lat').val() ]
+      },
+      properties: {
+         name: inputs.children('.name').val(),
+         type: inputs.children('.type').val(),
+         airGap: inputs.children('.airGap').val(),
+         areaCode: inputs.children('.areaCode').val(),
+   	   blockNumber: inputs.children('.blockNumber').val(),
+         field: inputs.children('.field').val(),
+   		waterDepth: inputs.children('.waterDepth').val(),
+   		workingInt: inputs.children('.workingInt').val(),
+   		pd: inputs.children('.pd').val(),
+   		rod: inputs.children('.rod').val(),
+   		sl: inputs.children('.sl').val(),
+   		oee: inputs.children('.oee').val(),
+   		lopi: inputs.children('.lopi').val(),
+   		windstormCSL: inputs.children('.windstormCSL').val(),
+   		windstormRet: inputs.children('.windstormRet').val()
+      }
+   };
+   console.log(asset);
+});
+
 // function makeArray() {
 //    var assetArr = [];
 //    var groups = $('.group');
@@ -26,6 +56,7 @@ $('.removeBtn').on('click', function(){
 // }
 
 function submitData(obj){
+   obj.preventDefault();
    var form = obj.parent();
    console.log(form);
    // var asset = {
